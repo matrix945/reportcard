82b43361ace63c8e54efed6b719904ed777d0988
matrix945 <frankzhang945@gmail.com>
2021-05-03 12:02:08 -0400

    Create local_reportcard.php
    zip installation need a language folder to support

---
+++ lang/en/local_reportcard.php
@@ -0,0 +1,16 @@
+
+
+/**
+ * Language File
+ *
+ * @package   local_reportcard
+ * @author    Frank
+ */
+
+
+/**************  by default strings used by the moodle  ****************/
+
+$string['modulename'] = "reportcard";
+$string['pluginname'] = 'reportcard';
+$string['modulename_help'] = '';
+$string['blank'] = '';
