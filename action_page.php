f02e5a8fedef66ce61eb5c5be7321f4ccca35090
matrix945 <frankzhang945@gmail.com>
2021-05-10 03:54:02 -0400

    fix email

--- action_page.php
+++ action_page.php
@@ -342,7 +342,7 @@ if(DEBUG){var_dump($obj);}
 if (checkErrorOrEmpty($obj)){
     $studentId = $obj[0]-> {'id'};
     $_SESSION['reportCard_studentId'] = $obj[0]-> {'id'};
-    $_SESSION['reportCard_studentEmail'] = $email;
+    $_SESSION['reportCard_studentEmail'] = $_POST["email"];
 }
 
 
